bf56fff1fb97bf46e0599ae67b24e592c41f0985
alltej <allan.tejano@gmail.com>
2017-06-24 12:44:01 -0400

    added DataParser tests

--- PowerPlantPowerConsumption/PowerPlantPowerConsumption/Plant.cs
+++ PowerPlantPowerConsumption/PowerPlantPowerConsumption/Plant.cs
@@ -1,6 +1,17 @@
-﻿namespace PowerPlantPowerConsumption
+﻿using System.Collections.Generic;
+using PowerPlanPowerConsumption.Tests;
+
+namespace PowerPlantPowerConsumption
 {
     public class Plant
     {
+        private List<KwhiTem> _kwhItems;
+
+        public List<KwhiTem> KwhItems => _kwhItems;
+
+        public void SetKwItems(List<KwhiTem> kwhItems)
+        {
+            _kwhItems = kwhItems;
+        }
     }
 }
